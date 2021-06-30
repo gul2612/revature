28adf712f3b265aafe0f06ec1903aecefe546ddd
gul2612 <gulgnit2612@gmail.com>
2021-06-30 22:28:40 +0530

    constructor

--- practice/src/practice/First.java
+++ practice/src/practice/First.java
@@ -2,28 +2,42 @@ package practice;
 
 public class First 
 {
-	private int x;
-	private int y;
-	public First(int x,int y)
+	private int id;
+	private String name;
+	private String city;
+	private int age;
+	public First(int id,String name)
 	{
-		this.x=x;
-		this.y=y;
+		this.id=id;
+		this.name=name;
 	}
-	public First(String s)
-	{
-		System.out.println("s="+s);
+	
+	public First(int id, String name, String city, int age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.city = city;
+		this.age = age;
 	}
-	public void add()
-	{
-		System.out.println("sum="+(x+y));
-	}
-	public void sub()
-	{
-		System.out.println("sub="+(x-y));
-	}
-	public void multiply()
-	{
-		System.out.println("multiply="+(x*y));
+	
+
+	
+
+	public void setCity(String city) {
+		this.city = city;
 	}
 
+	
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public void print()
+	{
+		System.out.println("id="+id);
+		System.out.println("name="+name);
+		System.out.println("city="+city);
+		System.out.println("age="+age);
+	}
 }
--- practice/src/practice/Firstmain.java
+++ practice/src/practice/Firstmain.java
@@ -3,11 +3,16 @@ package practice;
 public class Firstmain {
 
 	public static void main(String[] args) {
-		First c=new First(100,200);
-		c.add();
-		c.sub();
-		c.multiply();
-		First c1=new First("hello");
+		First e1=new First(100,"sachin");
+		System.out.println("printing e1");
+		e1.print();
+		First e2=new First(100,"saurav","kolkata",49);
+		System.out.println("printing e2");
+		e2.print();
+		e1.setCity("mumbai");
+		e1.setAge(45);
+		e1.print();
+		
 	}
 
 }
